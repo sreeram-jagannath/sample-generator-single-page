c33a0e9fba942235d65ad14ea00aa3826953ec78
Sreeram Jagannath <sreeram.subbaray@tigeranalytics.com>
2023-04-27 18:16:15 +0530

    formatted code

--- 1_Train.py
+++ 1_Train.py
@@ -35,12 +35,21 @@ def generate_data_demo():
             "./Pepsico App/synth_data/transaction_synth.xlsx"
         )
 
-        transaction_synth_df.SALES_VALUE = np.where(transaction_synth_df.SALES_VALUE>60 , transaction_synth_df.SALES_VALUE- 68,
-                                                  transaction_synth_df.SALES_VALUE)
-                                            
-        alt_value_sales = [random.triangular(0,10,1) for _ in range(0,transaction_synth_df.shape[0])]
+        transaction_synth_df.SALES_VALUE = np.where(
+            transaction_synth_df.SALES_VALUE > 60,
+            transaction_synth_df.SALES_VALUE - 68,
+            transaction_synth_df.SALES_VALUE,
+        )
 
-        transaction_synth_df.SALES_VALUE  = np.where(transaction_synth_df.SALES_VALUE <0, alt_value_sales,transaction_synth_df.SALES_VALUE  )
+        alt_value_sales = [
+            random.triangular(0, 10, 1) for _ in range(0, transaction_synth_df.shape[0])
+        ]
+
+        transaction_synth_df.SALES_VALUE = np.where(
+            transaction_synth_df.SALES_VALUE < 0,
+            alt_value_sales,
+            transaction_synth_df.SALES_VALUE,
+        )
 
         raw_synthetic_data_dict = {
             "product": product_synth_df,
@@ -55,22 +64,22 @@ def generate_data_demo():
     region_filter = st.session_state.get("region_filter")
     store_filter = st.session_state.get("store_type_filter")
 
-    synth_store = raw_synthetic_data_dict['store']
+    synth_store = raw_synthetic_data_dict["store"]
     new_store_synth = synth_store[
-            synth_store["STORE_TYPE"].isin(store_filter)
-            & synth_store["REGION"].isin(region_filter)
-        ]
-    
+        synth_store["STORE_TYPE"].isin(store_filter)
+        & synth_store["REGION"].isin(region_filter)
+    ]
+
     trans_synth = raw_synthetic_data_dict["transaction"]
     new_trans_synth = trans_synth[
-            trans_synth["STORE_ID"].isin(new_store_synth.STORE_ID)
-        ]
-    
+        trans_synth["STORE_ID"].isin(new_store_synth.STORE_ID)
+    ]
+
     prod_synth = raw_synthetic_data_dict["product"]
     new_prod_synth = prod_synth[
-            prod_synth["PRODUCT_ID"].isin(new_trans_synth.PRODUCT_ID)
-        ]
-    
+        prod_synth["PRODUCT_ID"].isin(new_trans_synth.PRODUCT_ID)
+    ]
+
     filtered_synth_data = {
         "store": new_store_synth,
         "transaction": new_trans_synth,
@@ -86,7 +95,11 @@ if __name__ == "__main__":
 
     header_ui(title="Synthetic Order Generation Tool")
 
-    all_files = st.file_uploader(label="Upload the store data, product data, tranasaction data and the metadata", accept_multiple_files=True, help="Select multiple files together and upload")
+    all_files = st.file_uploader(
+        label="Upload the store data, product data, tranasaction data and the metadata",
+        accept_multiple_files=True,
+        help="Select multiple files together and upload",
+    )
 
     st.divider()
 
@@ -122,9 +135,8 @@ if __name__ == "__main__":
             value=1.00,
             step=0.01,
             key="store_frac",
-            help="Fraction of rows that we want from the stores data"
+            help="Fraction of rows that we want from the stores data",
         )
-
 
         store_df = raw_store_df[
             raw_store_df["STORE_TYPE"].isin(STORE_TYPE_filter)
@@ -133,7 +145,9 @@ if __name__ == "__main__":
         store_df = store_df.sample(frac=store_frac, random_state=SEED)
 
         # read the transaction data
-        raw_transaction_df = pd.read_csv("./Pepsico App/real_data/transactions_store.csv")
+        raw_transaction_df = pd.read_csv(
+            "./Pepsico App/real_data/transactions_store.csv"
+        )
         transaction_df = raw_transaction_df[
             raw_transaction_df["STORE_ID"].isin(store_df.STORE_ID)
         ]
@@ -146,20 +160,25 @@ if __name__ == "__main__":
             product_df["PRODUCT_ID"].isin(transaction_df.PRODUCT_ID)
         ]
 
-        before_data_ct.markdown("<b> Data Shapes before <b>", unsafe_allow_html=True) 
+        before_data_ct.markdown("<b> Data Shapes before <b>", unsafe_allow_html=True)
         cl1, cl2, cl3 = before_data_ct.columns(3)
         cl1.write(f"Store: {raw_store_df.shape[0]:,} x {len(raw_store_df.columns)}")
-        cl2.write(f"Product: {raw_product_df.shape[0]:,} x {len(raw_product_df.columns)}")
-        cl3.write(f"Transaction: {raw_transaction_df.shape[0]:,} x {len(raw_transaction_df .columns)}")
+        cl2.write(
+            f"Product: {raw_product_df.shape[0]:,} x {len(raw_product_df.columns)}"
+        )
+        cl3.write(
+            f"Transaction: {raw_transaction_df.shape[0]:,} x {len(raw_transaction_df .columns)}"
+        )
 
-        st.markdown("<b> Data Shapes after <b>", unsafe_allow_html=True) 
+        st.markdown("<b> Data Shapes after <b>", unsafe_allow_html=True)
         cl4, cl5, cl6 = st.columns(3)
         cl4.write(f"Store: {store_df.shape[0]:,} x {len(store_df.columns)}")
         cl5.write(f"Product: {product_df.shape[0]:,} x {len(product_df.columns)}")
-        cl6.write(f"Transaction: {transaction_df.shape[0]:,} x {len(transaction_df .columns)}")
+        cl6.write(
+            f"Transaction: {transaction_df.shape[0]:,} x {len(transaction_df .columns)}"
+        )
 
         # st.divider()
-
 
         # 1. Collect all the tables in a dict
         real_data = {}
@@ -187,8 +206,6 @@ if __name__ == "__main__":
         if "generate_button" not in st.session_state:
             st.session_state["generate_button"] = False
 
-
-
         if bt1.button(label="Train Model") or st.session_state["train_button"]:
             st.session_state["train_button"] = not st.session_state["train_button"]
             if not st.session_state.get("model_trained", False):
@@ -197,29 +214,33 @@ if __name__ == "__main__":
 
             st.success("Model Trained! :fire:")
             st.session_state["model_trained"] = True
-        
+
         st.divider()
 
-
         if st.session_state.get("model_trained"):
-            st.markdown(body="<h2 align=center> Generate samples </h2>", unsafe_allow_html=True)
+            st.markdown(
+                body="<h2 align=center> Generate samples </h2>", unsafe_allow_html=True
+            )
 
             # Float scale value input
-            scale = st.number_input(label="Scale", value=2.0, step=0.1, help="Scale the number of rows in synthetic data")
+            scale = st.number_input(
+                label="Scale",
+                value=2.0,
+                step=0.1,
+                help="Scale the number of rows in synthetic data",
+            )
 
             _, bt2, _ = st.columns([2, 2, 1])  # train model button container
             if bt2.button(label="Generate Data"):
-                st.session_state["generate_button"] = not st.session_state["generate_button"]
+                st.session_state["generate_button"] = not st.session_state[
+                    "generate_button"
+                ]
                 # st.session_state["generate_button"] = True
 
-                
-
                 with st.spinner(text="Generating synthetic data"):
-
                     synth_data = generate_data_demo()
                     st.session_state["filtered_synth_data"] = synth_data
 
-                    
                     st.session_state["data_generated"] = True
 
                     quality_report = evaluate_quality(
@@ -254,10 +275,13 @@ if __name__ == "__main__":
             }
 
             col1, col2 = st.columns(2)
-            table_select = col1.selectbox(label="Select Table", options=options.keys(), index=0)
+            table_select = col1.selectbox(
+                label="Select Table", options=options.keys(), index=0
+            )
             column_select = col2.selectbox(
-                label="Select Column", options=options.get(table_select), index=0)
-            
+                label="Select Column", options=options.get(table_select), index=0
+            )
+
             if st.session_state.get("filtered_synth_data") is not None:
                 fig = get_column_plot(
                     real_data=st.session_state.get("real_data"),
